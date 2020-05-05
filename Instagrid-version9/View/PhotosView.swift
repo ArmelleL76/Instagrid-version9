ddb72c2f963dcf9a561b610feeea06ae08c94646
Armelle <armelle.lecerf@orange.fr>
2020-05-05 17:00:12 +0200

    ajout des classes PhotosView et SwipeView

---
+++ Instagrid-version9/View/PhotosView.swift
@@ -0,0 +1,21 @@
+//
+//  PhotosView.swift
+//  Instagrid-version9
+//
+//  Created by macmini-Armelle on 05/05/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import UIKit
+
+class PhotosView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Instagrid-version9/View/SwipeView.swift
@@ -0,0 +1,21 @@
+//
+//  SwipeView.swift
+//  Instagrid-version9
+//
+//  Created by macmini-Armelle on 05/05/2020.
+//  Copyright © 2020 armellelecerf. All rights reserved.
+//
+
+import UIKit
+
+class SwipeView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
