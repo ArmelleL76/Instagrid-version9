337817392341320b5f1543593cf402bd6ce01325
Armelle <armelle.lecerf@orange.fr>
2020-05-08 00:25:18 +0200

    ajout de l'image sélecte

--- Instagrid-version9/Controller/ViewController.swift
+++ Instagrid-version9/Controller/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var swipeToShareLabel: UILabel!
+    @IBOutlet weak var swipeToShareButton: UIButton!
+     @IBOutlet weak var layoutOneButton: UIButton!
+    @IBOutlet weak var layoutTwoButton: UIButton!
+    @IBOutlet weak var layoutThreeButton: UIButton!
+    @IBOutlet weak var topLeftGridButton: UIButton!
+    @IBOutlet weak var TopRightGridButton: UIButton!
+    @IBOutlet weak var bottomLeftGridButton: UIButton!
+    @IBOutlet weak var bottomRightGridButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+   
 }
 
